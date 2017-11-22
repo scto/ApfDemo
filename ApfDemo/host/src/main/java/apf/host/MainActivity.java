dd41c6770a87d10f145e24c250879a30cc4d81f5
liuchonghui <liuchonghui@xiaomi.com>
2017-11-22 13:34:24 +0800

    verified plugin install and intent launch.
    N/A
    
    Signed-off-by: liuchonghui <liuchonghui@xiaomi.com>

--- ApfDemo/host/src/main/java/apf/host/MainActivity.java
+++ ApfDemo/host/src/main/java/apf/host/MainActivity.java
@@ -1,10 +1,26 @@
 package apf.host;
 
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import com.limpoxe.fairy.content.PluginDescriptor;
+import com.limpoxe.fairy.manager.PluginManagerHelper;
+
+import java.io.File;
+import java.io.InputStream;
+import java.net.URISyntaxException;
+import java.util.List;
+
+import file.compact.FileCompactUtil;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,12 +31,111 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         final TextView tvTest = (TextView) findViewById(R.id.tv_test);
-        Button btnTest = (Button) findViewById(R.id.btn_test);
+        final Button btnTest = (Button) findViewById(R.id.btn_test);
+        btnTest.setEnabled(false);
         btnTest.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(View view) {
+                try {
+                    boolean exist = false;
+                    Intent intent = null;
+                    intent = new Intent("android.plugin.framework.launch_test_action");
+                    exist = checkIntentHasHandle(view.getContext(), intent);
+                    Log.d("PPP", "installPlugin|" + convertIntentToString(intent) + "|exist|" + exist);
+
+                    intent = new Intent(Intent.ACTION_VIEW);
+                    intent.setData(Uri.parse("mifg://android_plugin_framework/test?id=0"));
+
+                    exist = checkIntentHasHandle(view.getContext(), intent);
+                    // Intent intent = Intent.parseUri(strIntent, Intent.URI_INTENT_SCHEME);
+                    Log.d("PPP", "installPlugin|" + convertIntentToString(intent) + "|exist|" + exist);
+
+                    if (exist) {
+                        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                        startActivity(intent);
+                    }
+                } catch (Exception e) {
+                }
             }
         });
+
+        String patchDirPath = FileCompactUtil.getPatchDirPath(this);
+        FileCompactUtil.copyAssetsBySuffixToDir(this, ".apk", patchDirPath);
+
+        File[] files = FileCompactUtil.listDirFilesBySuffix(patchDirPath, ".apk");
+        for (File file : files) {
+            int resultCode = PluginManagerHelper.installPlugin(file.getAbsolutePath());
+            String message = getPluginErrMsg(resultCode);
+            Log.d("PPP", "installPlugin|" + file.getName() + "|" + message);
+        }
+
+        List<PluginDescriptor> plugins = PluginManagerHelper.getPlugins();
+        if (plugins != null && plugins.size() > 0) {
+            tvTest.setText("有插件");
+            btnTest.setEnabled(true);
+        }
     }
 
+    private static String getPluginErrMsg(int code) {
+        if(code== PluginManagerHelper.SUCCESS) {
+            return "成功";
+        } else if (code == PluginManagerHelper.SRC_FILE_NOT_FOUND) {
+            return "失败: 安装文件未找到";
+        } else if (code == PluginManagerHelper.COPY_FILE_FAIL) {
+            return "失败: 复制安装文件到安装目录失败";
+        } else if (code == PluginManagerHelper.SIGNATURES_INVALIDATE) {
+            return "失败: 安装文件验证失败";
+        } else if (code == PluginManagerHelper.VERIFY_SIGNATURES_FAIL) {
+            return "失败: 插件和宿主签名串不匹配";
+        } else if (code == PluginManagerHelper.PARSE_MANIFEST_FAIL) {
+            return "失败: 插件Manifest文件解析出错";
+        } if (code == PluginManagerHelper.FAIL_BECAUSE_SAME_VER_HAS_LOADED) {
+            return "失败: 同版本插件已加载,无需安装";
+        } else if (code == PluginManagerHelper.MIN_API_NOT_SUPPORTED) {
+            return "失败: 当前系统版本过低,不支持此插件";
+        } else if (code == PluginManagerHelper.PLUGIN_NOT_EXIST) {
+            return "失败: 插件不存在";
+        } else if (code == PluginManagerHelper.REMOVE_FAIL) {
+            return "失败: 删除插件失败";
+        } else if (code == PluginManagerHelper.HOST_VERSION_NOT_SUPPORT_CURRENT_PLUGIN) {
+            return "失败: 插件要求的宿主版本和当前宿主版本不匹配";
+        } else {
+            return "失败: 其他 code=" + code;
+        }
+    }
+
+    boolean checkIntentHasHandle(Context ctx, Intent intent) {
+        try {
+            boolean hasHandle = true;
+            if (ctx.getPackageManager().resolveActivity(intent,
+                    PackageManager.MATCH_DEFAULT_ONLY) == null) {
+                if (ctx.getPackageManager().resolveService(intent,
+                        PackageManager.MATCH_DEFAULT_ONLY) == null) {
+                    hasHandle = false;
+                }
+            }
+            return hasHandle;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    String convertIntentToString(Intent intent) {
+        try {
+            return intent.toUri(Intent.URI_INTENT_SCHEME);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    Intent convertStringToIntent(String string) {
+        try {
+            return Intent.parseUri(string, Intent.URI_INTENT_SCHEME);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
